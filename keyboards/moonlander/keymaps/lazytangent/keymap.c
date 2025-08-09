5213435f078e6b5abdc3b00d4485c33c0948b5b6
Peter Mai <peter.tn.mai@gmail.com>
2025-08-09 16:55:47 -0500

    Update moonlander mappings

--- keyboards/moonlander/keymaps/lazytangent/keymap.c
+++ keyboards/moonlander/keymaps/lazytangent/keymap.c
@@ -19,16 +19,18 @@
 #include QMK_KEYBOARD_H
 
 #define ALT_CTL A(KC_LCTL)
+#define SPC_LT1 LT(1, KC_SPC)
+#define ENT_LT2 LT(2, KC_ENT)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_moonlander(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_ESC,            MO(3),   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSLS,           KC_DEL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    MO(3),             KC_DEL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,           KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        KC_LCTL, KC_LALT, ALT_CTL, KC_LGUI, MO(1),            QK_LEAD,           QK_LEAD,          MO(2),   KC_RGUI, ALT_CTL, KC_RALT, KC_RCTL,
-                                            KC_SPC,  MO(2),   KC_LGUI,           MO(1),   KC_ENT,  KC_BSPC
+        KC_LCTL, KC_LALT, ALT_CTL, KC_LSFT, KC_LGUI,          QK_LEAD,           QK_LEAD,          MO(2),   KC_RGUI, KC_RSFT, KC_RALT, KC_RCTL,
+                                            SPC_LT1, MO(2),   KC_LGUI,           OSL(1),  ENT_LT2, KC_BSPC
     ),
 
     [1] = LAYOUT_moonlander(
@@ -36,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PIPE,           _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    QK_BOOT,
         MO(3),   KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN, KC_LCBR,           KC_RCBR, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,
         _______, _______, _______, KC_PIPE, KC_LCBR, KC_RCBR,                             KC_LBRC, KC_RBRC, KC_LT,   KC_GT,   KC_QUES, _______,
-        _______, _______, _______, _______, _______,          _______,           _______,          MO(3),   _______, _______, _______, MO(3),
+        _______, _______, _______, _______, _______,          _______,           CW_TOGG,          MO(3),   _______, _______, _______, MO(3),
                                             _______, MO(3),   _______,           _______, _______, KC_DEL
     ),
 
@@ -52,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT_moonlander(
         _______, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,            KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  QK_BOOT,
         RM_TOGG, RM_NEXT, RM_HUEU, RM_SATU, RM_VALU, RM_SPDU, _______,           _______, _______, _______, _______, _______, _______, _______,
-        _______, RM_PREV, RM_HUED, RM_SATD, RM_VALD, RM_SPDD, _______,           _______, _______, _______, _______, _______, _______, _______,
+        _______, RM_PREV, RM_HUED, RM_SATD, RM_VALD, RM_SPDD, _______,           _______, MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______,
         EE_CLR,  _______, _______, _______, _______,          _______,           _______,          _______, _______, _______, _______, _______,
                                             _______, _______, _______,           _______, _______, _______
--- users/lazytangent/rules.mk
+++ users/lazytangent/rules.mk
@@ -5,4 +5,7 @@ UNICODE_COMMON = yes
 LEADER_ENABLE = yes
 CAPS_WORD_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+
+# Command's default bindings (LSFT+RSFT) conflict with the default binding for
+# caps-word, so it's disabled until I find that I want to use it.
 COMMAND_ENABLE = no
