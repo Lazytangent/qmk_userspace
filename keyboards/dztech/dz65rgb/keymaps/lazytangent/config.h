1c12801e1f085c36917ded5c5ed06be3fd57d8ad
Peter Mai <peter.tn.mai@gmail.com>
2025-08-09 10:09:41 -0500

    Update dz65rgb keymap and config

--- keyboards/dztech/dz65rgb/keymaps/lazytangent/config.h
+++ keyboards/dztech/dz65rgb/keymaps/lazytangent/config.h
@@ -2,3 +2,11 @@
 #undef LOCKING_RESYNC_ENABLE
 #define NO_ACTION_TIMEOUT
 #define NO_MUSIC_MODE
+
+// azure
+#define RGB_MATRIX_DEFAULT_HUE 132
+#define RGB_MATRIX_DEFAULT_SAT 102
+#define RGB_MATRIX_DEFAULT_VAL 255
+
+/// Gets defined too late and info_config.h takes precedence
+// #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_BREATHING
