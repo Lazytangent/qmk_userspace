160db3d13d3e63eaf96d2bf2d8165874d5576d9a
Peter Mai <peter.tn.mai@gmail.com>
2025-08-10 13:18:31 -0500

    Update q8 keymap

--- keyboards/keychron/q8/ansi/keymaps/lazytangent/keymap.c
+++ keyboards/keychron/q8/ansi/keymaps/lazytangent/keymap.c
@@ -18,16 +18,13 @@
 
 // clang-format off
 
-#define KC_TASK LGUI(KC_TAB)
-#define KC_FLXP LGUI(KC_E)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ansi_69(
         KC_GRV,  KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,          KC_ESC,
         KC_TAB,  KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,          KC_DEL,
         KC_LCTL, KC_A,     KC_S,     KC_D,    KC_F,    KC_G,              KC_H,    KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,           KC_HOME,
         KC_LSFT,           KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,     KC_B,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
-        KC_LCTL, KC_LOPT,  KC_LCMD,           MO(2),            KC_SPC,   MO(3),            KC_BSPC,           KC_RCMD,            KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LOPT,  KC_LCMD,           KC_SPC,           MO(2),    MO(3),            KC_BSPC,           KC_RCMD,            KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT_ansi_69(
@@ -39,26 +36,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [2] = LAYOUT_ansi_69(
-        KC_GRV,  KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_UNDS,  KC_PLUS,  _______,          QK_BOOT,
-        _______, KC_1,     KC_2,     KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,    KC_0,     _______,  _______,  _______,          _______,
-        _______, KC_GRV,   KC_TILD,  KC_BSLS, KC_LPRN, KC_RPRN,           KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS,  KC_COLN,  KC_DQUO,  _______,          _______,
-        _______,           _______,  _______, KC_PIPE, KC_LCBR, KC_RCBR,  _______, KC_LBRC, KC_RBRC, KC_LT,    KC_GT,    _______,  _______, _______,
-        _______, _______,  _______,           _______,          _______,  MO(4),            _______,           _______,            _______, _______, _______
+        _______, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   QK_BOOT,          QK_BOOT,
+        _______, MS_BTN1,  MS_UP,    MS_BTN1, MS_WHLU, _______, _______,  _______, _______, _______, _______,  _______,  _______,  QK_BOOT,          _______,
+        _______, MS_LEFT,  MS_DOWN,  MS_RGHT, MS_WHLD, _______,           _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        _______,           _______,  MS_WHLL, MS_WHLR, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______, _______,
+        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______
     ),
 
     [3] = LAYOUT_ansi_69(
-        KC_GRV,  KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
-        _______, KC_EXLM,  KC_AT,    KC_HASH, KC_DLR,  KC_PERC, KC_CIRC,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_UNDS,  KC_PLUS,  _______,          _______,
-        _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______,  _______,  _______,          _______,
+        _______, _______,  _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        RM_TOGG, RM_NEXT,  RM_HUEU,  RM_SATU, RM_VALU, RM_SPDU, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
+        _______, RM_PREV,  RM_HUED,  RM_SATD, RM_VALD, RM_SPDD,           _______, _______, _______, _______,  _______,  _______,  _______,          _______,
         _______,           _______,  _______, _______, _______, _______,  _______, NK_TOGG, _______, _______,  _______,  _______,  _______, _______,
-        _______, _______,  _______,           MO(4),            _______,  _______,          _______,           _______,            _______, _______, _______
-    ),
-
-    [4] = LAYOUT_ansi_69(
-        KC_TILD, KC_F1,    KC_F2,    KC_F3,   KC_F4,   KC_F5,   KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,          _______,
-        RGB_TOG, RGB_MOD,  RGB_VAI,  RGB_HUI, RGB_SAI, RGB_SPI, _______,  _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______, RGB_RMOD, RGB_VAD,  RGB_HUD, RGB_SAD, RGB_SPD,           _______, _______, _______, _______,  _______,  _______,  _______,          _______,
-        _______,           _______,  _______, _______, _______, _______,  _______, _______, _______, _______,  _______,  _______,  _______, _______,
-        _______, _______,  _______,           _______,          _______,  _______,          _______,           _______,            _______, _______, _______
+        _______, _______,  _______,           MO(4),            _______,  _______,          MO(2),             _______,            _______, _______, _______
     )
 };
