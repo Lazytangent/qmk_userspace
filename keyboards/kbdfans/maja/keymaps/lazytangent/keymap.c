e47b5f3a73a891e2745c2d58820f50b24565f9d9
Peter Mai <peter.tn.mai@gmail.com>
2025-08-12 22:07:25 -0500

    Update maja keymap
    - Trying to use thumb keys more and reduce pinky usage with mods

--- keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
+++ keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
@@ -1,4 +1,11 @@
+// vim: shiftwidth=2
+
 #include QMK_KEYBOARD_H
+
+enum keycodes {
+  L01LSFT = QK_USER,
+  L02LSFT,
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT( /* Base */
@@ -6,21 +13,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_DEL,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    MO(1),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,
-    KC_LALT,          KC_LGUI, MO(1),            KC_SPC,           KC_BSPC,          MO(2),                     KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_LALT,          KC_LGUI, MO(1),            KC_SPC,           MO(2),            KC_RGUI,                   KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   [1] = LAYOUT( /* FN */
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT, _______,
     _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, QK_BOOT, _______,
     _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, _______, _______,          _______, _______,
-    _______, _______, _______, KC_PIPE, KC_LCBR, KC_RCBR, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______,
-    _______,          _______, _______,          _______,          KC_DEL,           MO(3),                     _______, _______, _______
+    _______, _______, L01LSFT, KC_PIPE, KC_LCBR, KC_RCBR, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______,
+    _______,          L01LSFT, _______,          _______,          MO(3),            _______,                   _______, _______, _______
   ),
   [2] = LAYOUT( /* FN2 */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, _______,
     _______, MS_BTN2, MS_UP,   MS_BTN1, MS_WHLU, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, _______,
     _______, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-    _______, _______, MS_WHLL, MS_WHLR, _______, _______, MO(3),   _______, _______, _______, _______, _______, _______, _______,
-    _______,          _______, MO(3),            _______,          _______,          _______,                   _______, _______, _______
+    _______, _______, MS_WHLL, MS_WHLR, _______, _______, L02LSFT, _______, _______, _______, _______, _______, _______, _______,
+    _______,          _______, MO(3),            _______,          _______,          MO(3),                     _______, _______, _______
   ),
   [3] = LAYOUT( /* FN3 */
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -30,6 +37,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,          _______, _______,          _______,          _______,          _______,                   _______, _______, _______
   ),
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case L01LSFT:
+      if (!record->event.pressed) {
+        del_mods(MOD_BIT(KC_LSFT));
+        layer_on(1);
+        return false;
+      }
+
+      layer_move(0);
+      add_mods(MOD_BIT(KC_LSFT));
+
+      return false;
+    case L02LSFT:
+      if (!record->event.pressed) {
+        del_mods(MOD_BIT(KC_LSFT));
+        layer_on(2);
+        return false;
+      }
+
+      layer_move(0);
+      add_mods(MOD_BIT(KC_LSFT));
+
+      return false;
+    default:
+      return true;
+  }
+}
 
 /* const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { */
 /*   [0] = LAYOUT( */
