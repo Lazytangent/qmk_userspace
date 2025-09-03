89f401a48bcbc2b0e1a1e781047fb80b59245c67
Peter Mai <peter.tn.mai@gmail.com>
2025-09-02 19:05:47 -0500

    Update maja keymap

--- keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
+++ keyboards/kbdfans/maja/keymaps/lazytangent/keymap.c
@@ -16,14 +16,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LALT,          KC_LGUI, MO(1),            KC_SPC,           MO(2),            KC_RGUI,                   KC_LEFT, KC_DOWN, KC_RIGHT
   ),
   [1] = LAYOUT( /* FN */
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT, _______,
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, _______, QK_BOOT, _______,
-    _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, _______, _______,          _______, _______,
-    _______, _______, L01LSFT, KC_PIPE, KC_LCBR, KC_RCBR, _______, KC_LBRC, KC_RBRC, _______, _______, _______, _______, _______,
+    KC_ESC,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS, KC_PLUS, QK_BOOT, _______,
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_LCBR, KC_RCBR, QK_BOOT, _______,
+    _______, KC_GRV,  KC_TILD, KC_BSLS, KC_LPRN, KC_RPRN, KC_UNDS, KC_MINS, KC_EQL,  KC_PLUS, KC_COLN, KC_DQUO,          _______, _______,
+    _______, _______, L01LSFT, KC_PIPE, KC_LCBR, KC_RCBR, _______, KC_LBRC, KC_RBRC, KC_LABK, KC_RABK, KC_QUES, _______, _______,
     _______,          L01LSFT, _______,          _______,          MO(3),            _______,                   _______, _______, _______
   ),
   [2] = LAYOUT( /* FN2 */
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, _______,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  QK_BOOT, _______,
     _______, MS_BTN2, MS_UP,   MS_BTN1, MS_WHLU, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT, _______,
     _______, MS_LEFT, MS_DOWN, MS_RGHT, MS_WHLD, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
     _______, _______, MS_WHLL, MS_WHLR, _______, _______, L02LSFT, _______, _______, _______, _______, _______, _______, _______,
@@ -66,13 +66,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return true;
   }
 }
-
-/* const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { */
-/*   [0] = LAYOUT( */
-/*     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, */
-/*     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, */
-/*     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, */
-/*     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, */
-/*     _______,          _______, _______,          _______,          _______,          _______,                   _______, _______, _______, */
-/*   ), */
-/* }; */
